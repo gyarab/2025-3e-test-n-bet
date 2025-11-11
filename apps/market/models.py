41b938649a362f4c037179bd66dc52bdae4348ca
Adrian Ibrahim <adrian.ibrahim.s@gyarab.cz>
2025-11-11 17:37:53 +0100

    fixtures

--- apps/market/models.py
+++ apps/market/models.py
@@ -1,13 +1,5 @@
 # apps/market/models.py
 from django.db import models
 
-class BtcPrice(models.Model):
-    symbol = models.CharField(max_length=10)
-    price = models.DecimalField(max_digits=20, decimal_places=8)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.symbol} {self.price}"
-
 class Asset(models.Model):
     pass
