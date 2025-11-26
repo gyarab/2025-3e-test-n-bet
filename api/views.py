08fce4e24d7a2d4f5078e5aee5cdd9856de9395d
Illia Husak <illia.husak.s@gyarab.cz>
2025-11-26 14:31:37 +0100

    refactor changes

--- api/views.py
+++ api/views.py
@@ -5,7 +5,10 @@ def get_trades(request):
     # Tady můžeš brát request.body nebo GET parametry, pokud budeš chtít filtrování
     req_json = '{}'  # placeholder, nebo request.body
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM vrat_trade(%s::json);", [req_json])
+        cursor.execute(
+            "SELECT * FROM vrat_trade(%s::json, %s::json, NOW());",
+            [req_json, '{}']
+        )
         result = cursor.fetchone()
 
     if result:
