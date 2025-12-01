5f71041cb4c68b9a1bd82fc35cc132bb871a4eea
Illia Husak <illia.husak.s@gyarab.cz>
2025-12-01 13:24:41 +0100

    Frontend struktura #2: Change the frontend architecture

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from apps.market.services import get_market_summary
+
 def home_view(request):
+    # summary = get_market_summary(['BTC/USDT', 'ETH/USDT', 'XRP/USDT', 'BNB/USDT', 'SOL/USDT'])
     return render(request, 'core/home.html')
