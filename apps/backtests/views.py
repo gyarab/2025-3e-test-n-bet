281242481cc8309ddef4338a0c0ae888ba56f492
Paralands <gusak.ilya07@gmail.com>
2025-12-25 16:02:02 +0100

    First steps of creating backtest page

--- apps/backtests/views.py
+++ apps/backtests/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-    
+@login_required  
 def backtests_home(request):
     return render(request, "backtests/backtests.html")
