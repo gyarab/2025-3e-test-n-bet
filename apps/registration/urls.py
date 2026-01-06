43fc47ce4ade07a5182d03f6faf8ddbd3eb6aa2d
Illia Husak <illia.husak.s@gyarab.cz>
2026-01-06 09:20:38 +0100

    Refactoring

--- apps/registration/urls.py
+++ apps/registration/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-from .api.views import register  # relativní import
+from .views import login_view, logout_view, register  # relativní import
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', login_view, name="login"),
     path('register/', register, name='register'),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
